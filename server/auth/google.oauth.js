7db8f4150a475106a32b31596bfe72ddd03bb36f
Philip <png262@gmail.com>
2015-10-22 16:35:48 -0400

    initial

---
+++ server/auth/google.oauth.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('google', {
+	scope: 'email'
+}));
+
+router.get('/callback', passport.authenticate('google', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new GoogleStrategy({
+	clientID: '247538473538-igl91f34f8o0a17ddlu7q8vgoi7vuvde.apps.googleusercontent.com',
+	clientSecret: '-b_xAgDizO_3gLjtBvihSxV8',
+	callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
+}, function (token, refreshToken, profile, done) { 
+	User.findOne({'google.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = profile.emails[0].value;
+			User.create({
+				email: email,
+				photo: profile.photos[0].value,
+				name: profile.displayName,
+				google: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
---
+++ server/auth/twitter.oauth.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter');
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('twitter'));
+
+router.get('/callback', passport.authenticate('twitter', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new TwitterStrategy({
+	consumerKey: 'K2QSONSWqV50cfUZy27m2mI1G',
+	consumerSecret: 'OxR0cKI6UP2wnkwXcPfW4mOgGmfRFVIoGdAwGeu0IpNzhk6eAn',
+	callbackURL: 'http://127.0.0.1:8080/auth/twitter/callback'
+}, function (token, refreshToken, profile, done) { 
+	User.findOne({'twitter.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			// twitter will not provide an email, so we'll just fake it
+			var email = [profile.username , 'fake-auther-email.com'].join('@');
+			User.create({
+				email: email,
+				photo: profile.photos[0].value,
+				name: profile.displayName,
+				twitter: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
